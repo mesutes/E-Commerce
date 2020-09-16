cf478894d808f6ac7adc4f1ebb0c841151762be9
mesut es <mesut.es@outlook.com>
2020-09-17 00:15:05 +0300

    Generic repository uygulandı.

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using E_Commerce.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repositories
+{
+    public class GenericRepository<TEntity> where TEntity:class, new()
+    {
+        public void Add(TEntity entity) 
+        {
+            using var context = new E_CommerceContext();
+            context.Set<TEntity>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entity) 
+        {
+            using var context = new E_CommerceContext();
+            context.Set<TEntity>().Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using var context = new E_CommerceContext();
+            context.Set<TEntity>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public List<TEntity> GetAll() 
+        {
+            using var context = new E_CommerceContext();
+            return context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetEntity(int id) 
+        {
+            using var context = new E_CommerceContext();
+            return context.Set<TEntity>().Find(id);
+        }
+
+
+    }
+}
